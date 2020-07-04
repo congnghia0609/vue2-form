742a378de20aed464bc147f7f5557498e9d28a53
congnghia0609 <congnghia0609@gmail.com>
2020-07-04 14:49:16 +0700

    first commit

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,94 @@
+<template>
+	<div id="employee-form">
+		<form @submit.prevent="handleSubmit">
+			<label>Employee name</label>
+			<input 
+				ref="first"
+				v-model="employee.name" 
+				type="text" 
+				:class="{'has-error': submitting && invalidName}"
+				@focus="clearStatus"
+				@keypress="clearStatus"
+				/>
+			<label>Employee email</label>
+			<input 
+				v-model="employee.email" 
+				type="text" 
+				:class="{'has-error': submitting && invalidEmail}"
+				@focus="clearStatus"
+				/>
+			<p v-if="error && submitting" class="error-message">
+				❗ Please fill out all required fields
+			</p>
+			<p v-if="success" class="success-message">
+				✅ Employee successfully added
+			</p>
+			<button>Add Employee</button>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'employee-form',
+	data() {
+		return {
+			submitting: false,
+			error: false,
+			success: false,
+			employee: {
+				name: '',
+				email: '',
+			},
+		}
+	},
+	methods: {
+		handleSubmit() {
+			this.submitting = true
+			this.clearStatus()
+			if (this.invalidName || this.invalidEmail) {
+				this.error = true
+				return
+			}
+			//console.log('testing handleSubmit')
+			// Emitting events to the parent
+			this.$emit('add:employee', this.employee)
+			this.$refs.first.focus()
+			this.employee = {
+				name: '',
+				email: '',
+			}
+			this.error = false
+			this.success = true
+			this.submitting = false
+		},
+		clearStatus() {
+			this.success = false
+			this.error = false
+		}
+	},
+	computed: {
+		invalidName() {
+			return this.employee.name === ""
+		},
+		invalidEmail() {
+			return this.employee.email === ""
+		},
+	},
+}
+</script>
+
+<style scoped>
+form {
+	margin-bottom: 2rem;
+}
+[class*='-massage'] {
+	font-weight: 500;
+}
+.error-message {
+	color: #d33c40;
+}
+.success-message {
+	color: #32a95d;
+}
+</style>
